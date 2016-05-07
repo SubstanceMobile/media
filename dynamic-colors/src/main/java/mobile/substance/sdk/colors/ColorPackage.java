5a86c609deaeff3d53eace43dd540d1169910343
AnimbusDev <avovk@substanceproject.net>
2016-05-07 10:54:58 -0400

    No more bold claims about finishing DynamicColors

--- dynamic-colors/src/main/java/mobile/substance/sdk/colors/ColorPackage.java
+++ dynamic-colors/src/main/java/mobile/substance/sdk/colors/ColorPackage.java
@@ -1,25 +1,64 @@
 package mobile.substance.sdk.colors;
 
+import static mobile.substance.sdk.colors.DynamicColorsConstants.ICON_COLOR_ACTIVE_DARK_BG;
+import static mobile.substance.sdk.colors.DynamicColorsConstants.ICON_COLOR_ACTIVE_LIGHT_BG;
+import static mobile.substance.sdk.colors.DynamicColorsConstants.ICON_COLOR_INACTIVE_DARK_BG;
+import static mobile.substance.sdk.colors.DynamicColorsConstants.ICON_COLOR_INACTIVE_LIGHT_BG;
+import static mobile.substance.sdk.colors.DynamicColorsConstants.TEXT_COLOR_DARK_BG;
+import static mobile.substance.sdk.colors.DynamicColorsConstants.TEXT_COLOR_DISABLED_DARK_BG;
+import static mobile.substance.sdk.colors.DynamicColorsConstants.TEXT_COLOR_DISABLED_LIGHT_BG;
+import static mobile.substance.sdk.colors.DynamicColorsConstants.TEXT_COLOR_LIGHT_BG;
+import static mobile.substance.sdk.colors.DynamicColorsConstants.TEXT_COLOR_SECONDARY_DARK_BG;
+import static mobile.substance.sdk.colors.DynamicColorsConstants.TEXT_COLOR_SECONDARY_LIGHT_BG;
+
 /**
  * Created by Julian Os on 04.05.2016.
  */
 public class ColorPackage {
-    private int primaryColor, primaryDarkColor, textColor, secondaryTextColor,
-        accentColor, accentTextColor, accentSecondaryTextColor;
+    private int primaryColor, primaryDarkColor, textColor, secondaryTextColor, disbledTextColor,
+        accentColor, accentTextColor, accentSecondaryTextColor, accentDisabledTextColor,
+        iconActiveColor, iconInactiveColor, accentIconActiveColor, accentIconInactiveColor;
 
-    public ColorPackage(int primaryColor, int primaryDarkColor, int textColor, int secondaryTextColor,
-                        int accentColor, int accentTextColor, int accentSecondaryTextColor) {
-
+    public ColorPackage(int primaryColor, int primaryDarkColor, int textColor, int secondaryTextColor, int disabledTextColor,
+                        int accentColor, int accentTextColor, int accentSecondaryTextColor, int accentDisabledTextColor,
+                        int iconActiveColor, int iconInactiveColor, int accentIconActiveColor, int accentIconInactiveColor) {
         //Main Colors
         this.primaryColor = primaryColor;
         this.primaryDarkColor = primaryDarkColor;
         this.textColor = textColor;
         this.secondaryTextColor = secondaryTextColor;
+        this.disbledTextColor = disabledTextColor;
 
         //Accent Colors
         this.accentColor = accentColor;
         this.accentTextColor = accentTextColor;
         this.accentSecondaryTextColor = accentSecondaryTextColor;
+        this.accentDisabledTextColor = accentDisabledTextColor;
+
+        //Icon colors
+        this.iconActiveColor = iconActiveColor;
+        this.iconInactiveColor = iconInactiveColor;
+        this.accentIconActiveColor = accentIconActiveColor;
+        this.accentIconInactiveColor = accentIconInactiveColor;
+    }
+
+    public ColorPackage(int primaryColor, int accentColor){
+        this(primaryColor, DynamicColorsUtil.generatePrimaryDark(primaryColor), accentColor);
+    }
+
+    public ColorPackage(int primaryColor, int primaryDarkColor, int accentColor) {
+        this(primaryColor, primaryDarkColor,
+                DynamicColorsUtil.isColorLight(primaryColor) ? TEXT_COLOR_LIGHT_BG : TEXT_COLOR_DARK_BG,
+                DynamicColorsUtil.isColorLight(primaryColor) ? TEXT_COLOR_SECONDARY_LIGHT_BG : TEXT_COLOR_SECONDARY_DARK_BG,
+                DynamicColorsUtil.isColorLight(primaryColor) ? TEXT_COLOR_DISABLED_LIGHT_BG : TEXT_COLOR_DISABLED_DARK_BG,
+                accentColor,
+                DynamicColorsUtil.isColorLight(accentColor) ? TEXT_COLOR_LIGHT_BG : TEXT_COLOR_DARK_BG,
+                DynamicColorsUtil.isColorLight(accentColor) ? TEXT_COLOR_SECONDARY_LIGHT_BG : TEXT_COLOR_SECONDARY_DARK_BG,
+                DynamicColorsUtil.isColorLight(accentColor) ? TEXT_COLOR_DISABLED_LIGHT_BG : TEXT_COLOR_DISABLED_DARK_BG,
+                DynamicColorsUtil.isColorLight(primaryColor) ? ICON_COLOR_ACTIVE_LIGHT_BG : ICON_COLOR_ACTIVE_DARK_BG,
+                DynamicColorsUtil.isColorLight(primaryColor) ? ICON_COLOR_INACTIVE_LIGHT_BG : ICON_COLOR_INACTIVE_DARK_BG,
+                DynamicColorsUtil.isColorLight(accentColor) ? ICON_COLOR_ACTIVE_LIGHT_BG : ICON_COLOR_ACTIVE_DARK_BG,
+                DynamicColorsUtil.isColorLight(accentColor) ? ICON_COLOR_INACTIVE_LIGHT_BG : ICON_COLOR_INACTIVE_DARK_BG);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -42,6 +81,10 @@ public class ColorPackage {
         return secondaryTextColor;
     }
 
+    public int getDisbledTextColor() {
+        return disbledTextColor;
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     // Accent Colors
     ///////////////////////////////////////////////////////////////////////////
@@ -56,5 +99,29 @@ public class ColorPackage {
 
     public int getAccentTextColor() {
         return accentTextColor;
+    }
+
+    public int getAccentDisabledTextColor() {
+        return accentDisabledTextColor;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Icons
+    ///////////////////////////////////////////////////////////////////////////
+
+    public int getIconActiveColor() {
+        return iconActiveColor;
+    }
+
+    public int getIconInactiveColor() {
+        return iconInactiveColor;
+    }
+
+    public int getAccentIconActiveColor() {
+        return accentIconActiveColor;
+    }
+
+    public int getAccentIconInactiveColor() {
+        return accentIconInactiveColor;
     }
 }
--- dynamic-colors/src/main/java/mobile/substance/sdk/colors/DynamicColorsConstants.java
+++ dynamic-colors/src/main/java/mobile/substance/sdk/colors/DynamicColorsConstants.java
@@ -1,17 +1,23 @@
 package mobile.substance.sdk.colors;
 
-import android.graphics.Color;
-
+/**
+ * This class contains material values for text and icon colors.
+ */
 public class DynamicColorsConstants {
-    //Text colors
-    public static final int TEXT_COLOR_DARK = 0xDE000000;
-    public static final int TEXT_COLOR_LIGHT = 0xFFFFFFFF;
-    public static final int TEXT_COLOR_SECONDARY_LIGHT = 0x8A000000;
-    public static final int TEXT_COLOR_SECONDARY_DARK = 0xB3FFFFFF;
 
-    //Accent text colors
-    public static final int ICON_COLOR_ACTIVE_LIGHT_BG = 0x8A000000;
-    public static final int ICON_COLOR_ACTIVE_DARK_BG = 0xFFFFFFFF;
-    public static final int ICON_COLOR_INACTIVE_LIGHT_BG = 0x42000000;
-    public static final int ICON_COLOR_INACTIVE_DARK_BG = 0x4DFFFFFF;
+    //Text colors
+    public static final int TEXT_COLOR_LIGHT_BG = 0xDE000000,
+            TEXT_COLOR_SECONDARY_LIGHT_BG = 0x8A000000,
+            TEXT_COLOR_DISABLED_LIGHT_BG = 0x61000000;
+
+    public static final int TEXT_COLOR_DARK_BG = 0xFFFFFFF,
+            TEXT_COLOR_SECONDARY_DARK_BG = 0xB3FFFFFF,
+            TEXT_COLOR_DISABLED_DARK_BG = 0x80FFFFFF;
+
+    //Icon colors
+    public static final int ICON_COLOR_ACTIVE_LIGHT_BG = 0x8A000000,
+            ICON_COLOR_INACTIVE_LIGHT_BG = 0x42000000;
+
+    public static final int ICON_COLOR_ACTIVE_DARK_BG = 0xFFFFFFFF,
+            ICON_COLOR_INACTIVE_DARK_BG = 0x4DFFFFFF;
 }
